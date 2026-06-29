955699f4614c482f2d276f919d72bc429cb825ff
dipanjansahaa <dipanjansaha2003@gmail.com>
2026-06-29 22:21:49 +0530

    Week 5 doc-assist config.py updated

--- week5/document-assistant/config.py
+++ week5/document-assistant/config.py
@@ -1,8 +1,26 @@
+# chunk_size = 500
 chunk_size = 800
+# overlap = 50
 overlap = 150
 
+# increasing chunk size and overlap to preserve context
+
+
+# model_name = "all-MiniLM-L6-v2"
 model_name = "BAAI/bge-small-en-v1.5"
+# model_name = "BAAI/bge-base-en-v1.5"
+# model_name = "BAAI/bge-base-en-v1.5"
+
+# using a retrieval-focused embedding model
+
 
 model = "llama3.2:3b"
+# model = "qwen2.5-coder:7b"
 
-k = 3
+# using llm model
+
+
+# k = 3
+k = 5
+
+# retrieving more candidate chunks
