a0b89572e055e74f2511a6dd7a310de26bbb9d29
dipanjansahaa <dipanjansaha2003@gmail.com>
2026-08-13 00:18:07 +0530

    langchain basic model, message sent-received, metadata updated

--- practice2/01_basic_model/app2.py
+++ practice2/01_basic_model/app2.py
@@ -1,32 +1,31 @@
-# basic chat
 from langchain_ollama import ChatOllama
+from langchain_core.messages import HumanMessage
 
 
-def chat(model, temperature):
+def chat(model, temperature, content):
     model = ChatOllama(
         model = model,
         temperature = temperature
     )
 
-    response = model.invoke("What is SAP BW/4HANA? Explain in two sentences.")
+    messages = [
+        HumanMessage(
+            content=content
+        )
+    ]
 
-    # print(response, '\n')
+    response = model.invoke(messages)
 
     print(response.content)
 
-    # print(type(response))
-
     print()
-    # print(response.response_metadata["created_at"])
     print(response.response_metadata["model_name"])
-    # print(response.response_metadata["model_provider"])
 
     print(response.response_metadata)
     print(response.usage_metadata)
 
     print()
     total_duration = round(int(response.response_metadata["total_duration"]) / 10 ** 9, 3)
-    load_duration = round(int(response.response_metadata["load_duration"]) / 10 ** 9, 3)
     prompt_eval_duration = round(int(response.response_metadata["prompt_eval_duration"]) / 10 ** 9, 3)
     eval_duration = round(int(response.response_metadata["eval_duration"]) / 10 ** 9, 3)
 
@@ -42,9 +41,4 @@ def chat(model, temperature):
 
     print()
 
-chat(model="llama3.2:3b", temperature=0)
-chat(model="llama3.2:3b", temperature=0.4)
-chat(model="llama3.2:3b", temperature=0.8)
-chat(model="llama3.2:3b", temperature=0.8)
-
-chat(model="qwen2.5-coder:7b", temperature=0)
+chat(model="llama3.2:3b", temperature=0, content="What is SAP BW/4HANA? Explain in two sentences.")
